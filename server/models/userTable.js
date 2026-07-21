588d5e9a8cee9176cd01abc68a13e218463edb6a
Pratham Gupta <prathamgupta0405@gmail.com>
2026-07-21 23:59:05 +0530

    add product and user table creation scripts

---
+++ server/models/userTable.js
@@ -0,0 +1,24 @@
+import pool from "../database/db.js";
+
+export async function createUserTable() {
+  try {
+    const query = `
+            CREATE TABLE IF NOT EXISTS users(
+                id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+                name VARCHAR(100) NOT NULL CHECK (char_length(name) >= 3), 
+                email VARCHAR(100) UNIQUE NOT NULL, 
+                password TEXT NOT NULL, 
+                role VARCHAR(10) DEFAULT 'User' CHECK (role IN ('User', 'Admin')),
+                avatar JSONB DEFAULT NULL,
+                reset_password_token TEXT DEFAULT NULL,
+                reset_password_expire TIMESTAMP DEFAULT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `;
+        await pool.query(query);
+        console.log("Users table created successfully")
+  } catch (err) {
+    console.log("Error creating user table:", err);
+    process.exit(1);
+  }
+}
