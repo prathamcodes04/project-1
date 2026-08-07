46eecb56f8224a35dc3594d00925b86110f9622e
Pratham Gupta <prathamgupta0405@gmail.com>
2026-08-07 22:23:57 +0530

    feat: implement payment intent generation and database storage

--- server/utils/generatePayementIntent.js
+++ server/utils/generatePayementIntent.js
@@ -1,10 +1,25 @@
 import stripe from "../config/stripe.js"
 import pool from "../database/db.js";
 
-export const generatePaymentIntent = async(req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
+export async function generatePayementIntent(orderId, totalPrice) {
+   try {
+      // create payment intent
+      const paymentIntent = await stripe.paymentIntents.create({
+         amount: Math.round(totalPrice * 100),
+         currency: "inr",
+      });
+
+      // save payment details
+      await pool.query(
+         `INSERT INTO payments
+         (order_id, payment_type, payment_status, payment_intent_id)
+         VALUES ($1, $2, $3, $4) RETURNING *`,
+         [orderId, "Online", "Pending", paymentIntent.client_secret]
+      );
+
+      return {success: true, clientSecret: paymentIntent.id};
+   } catch (error) {
+      console.error("Payment error:", error.message || error);
+      return {success: false, message: "Payment failed"};      
+   }
 }
